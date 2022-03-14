fe9049db6304255f5bc8e8cdbc08c74fdb04e986
kunal wahule <Kunalskw143@gmail.com>
2022-03-14 17:45:19 +0530

    SplashScreen

---
+++ app/src/main/java/com/example/e_mentoring/Login.java
@@ -0,0 +1,20 @@
+package com.example.e_mentoring;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void Direct(View view) {
+    }
+    public void ForgetPassword(View view) {
+    }
+}
